edb6f8166e3e8352f5deb573a9d34e85ae517197
Mehdi Ayache <cornlyouns@gmail.com>
2019-03-19 23:23:44 +0100

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="md-layout" id="#login">
+    <div class="cookie-container"><img src="~@/assets/pics/cookie.png"></div>
+    <md-card class="md-layout-item md-size-20 md-small-size-100 login">
+      <md-card-header>
+        <div class="md-title">CookieTouch</div>
+      </md-card-header>
+
+      <md-card-content>
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label>Email</label>
+              <md-input v-model="initial"></md-input>
+            </md-field>
+          </div>
+        </div>
+        <div class="md-layout md-gutter">
+          <div class="md-layout-item md-small-size-100">
+            <md-field>
+              <label>Mot de passe</label>
+              <md-input v-model="initial" type="password"></md-input>
+            </md-field>
+          </div>
+        </div>
+      </md-card-content>
+
+      <md-card-actions>
+        <md-button>Se connecter</md-button>
+      </md-card-actions>
+    </md-card>
+
+
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+
+</script>
+<style lang="css">
+  .login {
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    background: rgba(0, 0, 0, .6) !important;
+    border-radius: 8px;
+    color: whitesmoke !important;
+  }
+
+  .cookie-container {
+    position: relative;
+    width: 100%
+  }
+
+  .cookie-container>img {
+    width: 150px;
+    height: 150px;
+  }
+
+  #app {
+    background: url('~@/assets/pics/login_bg.jpg') no-repeat center fixed;
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+    /* background: red; */
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
 Vue.use(Router)
+Vue.use(VueMaterial)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/dashboard',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: Login
     }
   ]
 })
