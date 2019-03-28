6b767946062203e20dcef6d549043aef3126aa89
Mehdi Ayache <ayache@kizeo.com>
2019-03-28 11:04:34 +0100

    new

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,138 +1,180 @@
 <template>
-  <div class="md-layout" id="#login">
-    <div class="cookie-container">
-      <img src="~@/assets/pics/cookie.png">
+    <div class="md-layout" id="#login">
+        <div class="cookie-container">
+            <img src="~@/assets/pics/cookie.png">
+        </div>
+
+        <md-card class="md-layout-item md-size-20 md-small-size-100 login">
+            <md-card-header>
+                <div class="md-title">CookieTouch</div>
+            </md-card-header>
+
+            <md-card-content>
+                <div class="md-layout md-gutter">
+                    <div class="md-layout-item md-small-size-100">
+                        <md-field class="has-danger">
+                            <label>Email</label>
+                            <md-input v-model="email"></md-input>
+                        </md-field>
+                    </div>
+                </div>
+                <div class="md-layout md-gutter">
+                    <div class="md-layout-item md-small-size-100">
+                        <md-field>
+                            <label>Mot de passe</label>
+                            <md-input v-model="psw" type="password"></md-input>
+                        </md-field>
+                    </div>
+                </div>
+            </md-card-content>
+
+            <md-card-actions>
+                <md-button @click="signin()">Se connecter</md-button>
+            </md-card-actions>
+        </md-card>
     </div>
-
-    <md-card class="md-layout-item md-size-20 md-small-size-100 login">
-      <md-card-header>
-        <div class="md-title">CookieTouch</div>
-      </md-card-header>
-
-      <md-card-content>
-        <div class="md-layout md-gutter">
-          <div class="md-layout-item md-small-size-100">
-            <md-field class="has-danger">
-              <label>Email</label>
-              <md-input v-model="email"></md-input>
-            </md-field>
-          </div>
-        </div>
-        <div class="md-layout md-gutter">
-          <div class="md-layout-item md-small-size-100">
-            <md-field>
-              <label>Mot de passe</label>
-              <md-input v-model="psw" type="password"></md-input>
-            </md-field>
-          </div>
-        </div>
-      </md-card-content>
-
-      <md-card-actions>
-        <md-button @click="signin()">Se connecter</md-button>
-      </md-card-actions>
-    </md-card>
-  </div>
 </template>
 <script>
-  import firebase from "firebase"
-  import axios from "axios"
-  export default {
+import firebase from "firebase";
+import axios from "axios";
+import primus from "./primus";
+import SocketIO from "socket.io-client";
+export default {
     data() {
-      return {
-        email: "",
-        psw: ""
-      };
+        return {
+            email: "",
+            psw: ""
+        };
     },
     methods: {
-      randomString: function (len, bits = 36) {
-        let outStr = "";
-        let newStr;
-        while (outStr.length < len) {
-          newStr = Math.random()
-            .toString(bits)
-            .slice(2);
-          outStr += newStr.slice(0, Math.min(newStr.length, len - outStr.length));
+        randomString: function(len, bits = 36) {
+            let outStr = "";
+            let newStr;
+            while (outStr.length < len) {
+                newStr = Math.random()
+                    .toString(bits)
+                    .slice(2);
+                outStr += newStr.slice(
+                    0,
+                    Math.min(newStr.length, len - outStr.length)
+                );
+            }
+            return outStr.toUpperCase();
+        },
+        signin: function() {
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.email, this.psw)
+                .then(() => {
+                    console.log(" yes");
+                })
+                .catch(er => console.log(er));
+        },
+        createSocket: function(url) {
+            // let socket = SocketIO(url);
+            let socket = new WebSocket(
+                "wss://proxyconnection.touch.dofus.com",
+                "protocolOne"
+            );
+            socket.on("open", () => {
+                console.log("kkd");
+                return this;
+            });
+            // return new primus(url, {
+            //     manual: true,
+            //     reconnect: {
+            //         max: 5000,
+            //         min: 500,
+            //         retries: 10
+            //     },
+            //     strategy: "disconnect,timeout",
+            //     transformer: "engine.io",
+            //     transport: {}
+            // });
+        },
+        makeSticky: function(url, sessionId) {
+            const sep = url.indexOf("?") === -1 ? "?" : "&";
+            return url + sep + "STICKER=" + encodeURIComponent(sessionId);
+        },
+        initialize: function() {
+            const config = {
+                apiKey: "AIzaSyDWK5MrCUhiluOfj8emZT_ARUpbkzbwKTE",
+                authDomain: "cookietouch-52c0c.firebaseapp.com",
+                databaseURL: "https://cookietouch-52c0c.firebaseio.com",
+                messagingSenderId: "423749577733",
+                projectId: "cookietouch-52c0c",
+                storageBucket: "cookietouch-52c0c.appspot.com"
+            };
+            return firebase.initializeApp(config);
         }
-        return outStr.toUpperCase();
-      },
-      signin: function () {
-        firebase.auth().signInWithEmailAndPassword(this.email, this.psw).then(() => {
-          console.log(" yes");
-
-        }).catch(er => console.log(er))
-      },
-      initialize: function () {
-        const config = {
-          apiKey: 'AIzaSyDWK5MrCUhiluOfj8emZT_ARUpbkzbwKTE',
-          authDomain: 'cookietouch-52c0c.firebaseapp.com',
-          databaseURL: 'https://cookietouch-52c0c.firebaseio.com',
-          messagingSenderId: '423749577733',
-          projectId: 'cookietouch-52c0c',
-          storageBucket: 'cookietouch-52c0c.appspot.com',
-        }
-        return firebase.initializeApp(config)
-      }
     },
     mounted() {
-      this.initialize()
+        this.initialize();
 
-      axios.post("https://haapi.ankama.com/json/Ankama/v2/Api/CreateApiKey",
-        "login=Kasiope&password=krapazor84&long_life_token=true").then((d) => {
-        let key = d.data.key
-        console.log(key);
+        axios
+            .post(
+                "https://haapi.ankama.com/json/Ankama/v2/Api/CreateApiKey",
+                "login=kapumain&password=krapazor84&long_life_token=true"
+            )
+            .then(d => {
+                let key = d.data.key;
+                console.log(key);
 
-        axios.get("https://haapi.ankama.com/json/Ankama/v2/Account/CreateToken", {
-          headers: {
-            apikey: key
-          },
-          params: {
-            game: 18
-          }
-        }).then((d) => {
-          let dataUrl = "https://proxyconnection.touch.dofus.com"
-          let sessionid = randomString(16)
-
-        }).catch(err => console.log(err))
-
-      }).catch((err) => {
-        console.log(err);
-
-      })
-    },
-  };
-
+                axios
+                    .get(
+                        "https://haapi.ankama.com/json/Ankama/v2/Account/CreateToken",
+                        {
+                            headers: {
+                                apikey: key
+                            },
+                            params: {
+                                game: 18
+                            }
+                        }
+                    )
+                    .then(d => {
+                        let dataUrl = "https://proxyconnection.touch.dofus.com";
+                        let sessionid = this.randomString(16);
+                        let currentUrl = this.makeSticky(dataUrl, sessionid);
+                        let socket = this.createSocket(currentUrl);
+                    })
+                    .catch(err => console.log(err));
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+};
 </script>
 <style lang="css">
-  .md-field.md-theme-default::before {
+.md-field.md-theme-default::before {
     background-color: #eec485;
-  }
+}
 
-  .login {
+.login {
     position: relative;
     left: 50%;
     transform: translateX(-50%);
     background: rgba(0, 0, 0, 0.6) !important;
     border-radius: 8px;
     color: whitesmoke !important;
-  }
+}
 
-  .cookie-container {
+.cookie-container {
     position: relative;
     width: 100%;
-  }
+}
 
-  .cookie-container>img {
+.cookie-container > img {
     width: 150px;
     height: 150px;
-  }
+}
 
-  #app {
+#app {
     background: url("~@/assets/pics/login_bg.jpg") no-repeat center fixed;
     background-size: cover;
     height: 100vh;
     width: 100vw;
     /* background: red; */
-  }
-
+}
 </style>
